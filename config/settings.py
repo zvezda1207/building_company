21320b1f6db49100ab68401ff9ab2ad22b01bb28
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2026-04-26 20:35:51 +0300

    enable debug for media

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$t*9mjlq0-3d(slhyt%^_x*cq#u+@e+@*^w8!yx^)%a($yyr&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- config/urls.py
+++ config/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
